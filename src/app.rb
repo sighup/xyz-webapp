5654defcd9c1df7bdb0ca1cebab0525f7b34e817
Matt Saliano <sighup@gmail.com>
2023-02-18 16:34:23 -0600

    Change the message

--- src/app.rb
+++ src/app.rb
@@ -5,6 +5,6 @@ set :bind, '0.0.0.0'
 set :port, ENV['PORT'] || 4567
 
 get '/' do
-  json :message => 'Automate several of the things!',
+  json :message => 'Automate the things!',
        :timestamp => Time.now.to_i
 end
